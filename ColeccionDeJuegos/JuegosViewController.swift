d689f6fc6c5ecb53e41a3bc8dc94de548efc2510
Johan Miranda <johan.miranda@tecsup.edu.pe>
2018-05-03 20:20:30 -0500

    Final Commit

--- ColeccionDeJuegos/JuegosViewController.swift
+++ ColeccionDeJuegos/JuegosViewController.swift
@@ -1,8 +1,10 @@
 
 import UIKit
 
-class JuegosViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+class JuegosViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
+     let categories = ["Hack and Slash","RPG","SandBox"]
+    @IBOutlet weak var txtPicker: UITextField!
+    
     @IBOutlet weak var JuegoImageView: UIImageView!
     @IBOutlet weak var tituloTextField: UITextField!
     @IBOutlet weak var agregarActualizarBoton: UIButton!
@@ -14,9 +16,14 @@ class JuegosViewController: UIViewController, UIImagePickerControllerDelegate, U
         super.viewDidLoad()
         imagePicker.delegate = self
         
+        let pickerView = UIPickerView()
+        pickerView.delegate = self
+        txtPicker.inputView = pickerView
+        
         if juego != nil {
             JuegoImageView.image = UIImage(data: (juego!.imagen!) as Data)
             tituloTextField.text = juego!.titulo
+            txtPicker.text = juego!.categoria
             agregarActualizarBoton.setTitle("Actualizar", for: .normal)
         }else{
             eliminarBoton.isHidden = true
@@ -25,6 +32,22 @@ class JuegosViewController: UIViewController, UIImagePickerControllerDelegate, U
     @IBAction func cameraTapped(_ sender: Any) {
     }
     
+    //Functions Combo box
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return categories.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return categories[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        txtPicker.text = categories[row]
+    }
+    //
+    
     @IBAction func fotosTapped(_ sender: Any) {
         imagePicker.sourceType = .photoLibrary
         present(imagePicker, animated: true, completion: nil)
@@ -32,11 +55,13 @@ class JuegosViewController: UIViewController, UIImagePickerControllerDelegate, U
     @IBAction func agregarTapped(_ sender: Any) {
         if juego != nil {
             juego!.titulo = tituloTextField.text
+            juego!.categoria = txtPicker.text
             juego!.imagen = UIImagePNGRepresentation(JuegoImageView.image!) as Data?
         }else{
         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
         let juego = Juego(context: context)
         juego.titulo = tituloTextField.text
+        juego.categoria = txtPicker.text
         juego.imagen = UIImagePNGRepresentation(JuegoImageView.image!) as Data?
         }
         (UIApplication.shared.delegate as! AppDelegate).saveContext()
@@ -54,5 +79,6 @@ class JuegosViewController: UIViewController, UIImagePickerControllerDelegate, U
         (UIApplication.shared.delegate as! AppDelegate).saveContext()
         navigationController!.popViewController(animated: true)
     }
+    
     
 }
--- ColeccionDeJuegos/ViewController.swift
+++ ColeccionDeJuegos/ViewController.swift
@@ -19,19 +19,24 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.delegate = self
     }
     
+    @IBAction func editCells(_ sender: Any) {
+        self.tableView.isEditing = true
+    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return juegos.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        self.tableView.isEditing = false
         let cell = UITableViewCell()
         let juego = juegos[indexPath.row]
-        cell.textLabel?.text = juego.titulo
+        cell.textLabel?.text = juego.titulo!+"-> \(juego.categoria ?? "No")"
         cell.imageView?.image = UIImage(data: (juego.imagen!) as Data)
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
         let juego = juegos[indexPath.row]
         performSegue(withIdentifier: "juegoSegue", sender: juego)
     }
@@ -46,6 +51,28 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let siguienteVC = segue.destination as! JuegosViewController
         siguienteVC.juego = sender as? Juego
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            context.delete(juegos[indexPath.row])
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            juegos.remove(at: indexPath.row)
+            self.tableView.isEditing = false
+            tableView.reloadData()
+        }
+    }
+    func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+        let objectMovido = self.juegos[fromIndexPath.row]
+        juegos.remove(at: fromIndexPath.row)
+        juegos.insert(objectMovido, at: to.row)
+        NSLog("%@", "\(fromIndexPath.row) => \(to.row) \(juegos)")
+        self.tableView.isEditing = false
     }
 
 }
